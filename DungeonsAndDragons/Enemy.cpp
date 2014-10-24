39198133fa7eb187d179f3480debbdc59dc6fbaa
Sjoerd Nijhof <Sjoerdnijhof@hotmail.com>
2014-10-24 10:02:25 +0200

    De enemy class is vollediger gemaakt

--- DungeonsAndDragons/Enemy.cpp
+++ DungeonsAndDragons/Enemy.cpp
@@ -10,3 +10,55 @@ Enemy::Enemy()
 Enemy::~Enemy()
 {
 }
+
+// Getters
+std::string Enemy::GetName()
+{
+	return name;
+}
+
+int Enemy::GetLevel()
+{
+	return level;
+}
+
+int Enemy::GetHp()
+{
+	return hp;
+}
+
+int Enemy::GetXp()
+{
+	if (hp == 0)
+		return xp;
+	else
+		return 0;
+}
+
+// Functions
+int Enemy::Attack()
+{
+	// Gebruik een random getal, of de enemy mist of niet?
+	if (50 < 80) // <-- onzin getallen atm...
+	{
+		// De enemy raakt de tegenstander
+		// Bereken het aantal damage dat de tegenstander doet op basis van het level en een random.
+		int damage = 0;
+		return damage;
+	}
+	else
+	{
+		return 0;// The enemy missed
+	}
+}
+
+int Enemy::Hit(int damage)
+{
+	if (hp > damage)
+		hp = hp - damage;
+	else
+		hp = 0;
+
+	// Return the leftover hp of the enemy
+	return hp;
+}
--- DungeonsAndDragons/Enemy.h
+++ DungeonsAndDragons/Enemy.h
@@ -1,10 +1,25 @@
 #pragma once
+
 #include "Character.h"
 
 class Enemy : public Character
 {
+private:
+	std::string name;
+	int level;
+	int hp;
+	int xp;
+
 public:
 	Enemy();
 	~Enemy();
+
+	// Functions
+	std::string GetName();
+	int GetLevel();
+	int GetHp();
+	int GetXp();
+	int Attack();
+	int Hit(int damage);
 };
 
